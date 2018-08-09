ff9be18f3fd846961622d52aaa0f3352faf2e677
Jose Herrera <jherrera@localhost>
2018-08-09 14:44:48 -0700

    rename config vars

--- scripts/blog-index-generator.js
+++ scripts/blog-index-generator.js
@@ -1,12 +1,13 @@
 'use strict';
 
 hexo.extend.generator.register('blog', function (locals) {
-    var path = (hexo.config.pillars_blog_path || hexo.theme.config.pillars_blog_path);
+    var path = (hexo.config.pillars_path || hexo.theme.config.pillars_path);
     if (path.substr(-1) !== '/') {
         path += '/';
     }
 
-    var title = (hexo.config.pillars_blog_title|| hexo.theme.config.pillars_blog_title);
+    var title = (hexo.config.pillars_title|| hexo.theme.config.pillars_title);
+    var layouts = ['blog', 'index'].concat(hexo.config.pillars_layout|| hexo.theme.config.pillars_layout)
 
     locals = Object.assign({}, locals, {title: title});
     return {
